3dcbc2087f3a4b2d4d612805cee54e51efdbdd05
Marcia Carvalho <marciacarvalho1205@gmail.com>
2024-02-21 07:45:10 -0300

    segundo commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import HomePage from './src/pages/HomePage'
+
+const App = () => {
+
+ return (
+
+   <HomePage />
+
+ );
+
+}
+
+export default App;
+
+
+ 
+
